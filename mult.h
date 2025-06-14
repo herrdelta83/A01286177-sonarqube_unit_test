81926552bd21a74754ec44b01680f806eb40695c
herrdelta83 <bailondelta@gmail.com>
2025-06-13 23:20:22 -0600

    add mult header

---
+++ mult.h
@@ -0,0 +1,14 @@
+//
+// Created by 52812 on 13-Jun-25.
+//
+
+#include <math.h>
+#include <stdexcept>
+/**
+ Metodo para multiplicar dos numeros
+*/
+
+double mult(int x, int y)
+{
+    return x * y;
+}
