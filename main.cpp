234df9cc859ed058ae519f2eae0311fbe8daaed4
herrdelta83 <bailondelta@gmail.com>
2025-06-13 23:22:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,6 @@ int main() {
     std::cout << "sum(1, 5) = " << sum(1, 5) << '\n';
     std::cout << "sub(5, 1) = " << sub(5, 1) << '\n';
     std::cout << "div(9, 3) = " << divide(9, 3) << '\n';
-    std::cout << "mult(1, 6) = " << mult(1, 6) << '\n';
+    std::cout << "mult(1, 5) = " << mult(1, 5) << '\n';
     return 0;
 }
